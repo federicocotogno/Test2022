d907ffc567b2a3f15c0a3d3f8bff7b155af0da54
Federico <codepalace@fastmail.se>
2022-04-21 21:25:42 +0200

    Initial Commit

---
+++ Test2022/Test2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2022App.swift
+//  Test2022
+//
+//  Created by Federico on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
